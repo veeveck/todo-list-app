2488bc7383ff73ea5ffbf298d21ad67e553beea2
Vivek <veeveckjeet@gmail.com>
2023-04-20 23:10:40 -0400

    Edit functioanlity added

--- src/App.js
+++ src/App.js
@@ -4,8 +4,19 @@ import './App.css';
 function App() {
   const[todo,setTodo]=useState("");
   const[todos,setTodos]=useState([{id:1,todo:"Learn React"}]);
+  const [editId,setEditId]=useState(0);
   const handleSubmit=(e)=>{
     e.preventDefault();
+    if(editId){
+      const index=todos.findIndex(item=>item.id===editId);
+      const newTodos=[...todos];
+      newTodos.splice(index,1,{id:editId,todo});
+      
+      setTodos(newTodos);
+      setEditId(0);
+      setTodo('');
+      return;
+    }
     if(todo!==''){
     setTodos([...todos,{id:`${todo}-${Date.now()}`,todo}]);
     }
@@ -18,19 +29,24 @@ function App() {
     const delTodo=todos.filter(item=>item.id !== id);
     setTodos(delTodo);
   }
+  const handleEdit=(id)=>{
+   const editTodo=todos.find(item=>item.id ===id);
+   setTodo(editTodo.todo);
+   setEditId(id);
+  }
   return (
     <div className="App">
         <div className='container'>
           <h1>Advanced Todo List App</h1>
           <form className='formtodo' onSubmit={handleSubmit}>
             <input type="text" value={todo} onChange={handleTodo}/>
-            <button type="submit">Go</button>
+            <button type="submit">{editId?"Edit":"Go"}</button>
           </form>
           <ul className='listAll'>
           {todos.map((t)=>(
             <li className='todo'>
             <span className='todoName' key={t.id}>{t.todo}</span>
-            <button>Edit</button>
+            <button onClick={()=>handleEdit(t.id)}>Edit</button>
             <button onClick={()=>handleDelete(t.id)}>Delete</button>
             </li>
           ))}
